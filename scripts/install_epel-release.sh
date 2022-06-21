6abc6fddc133163291d2d683798c5d4ec77717bd
diego <dcmikki@gmail.com>
2022-06-21 01:49:46 +0100

    WIP

--- scripts/install_epel-release.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "Adding Extra Packages for Enterprise Linux 7 - x86_64"
-yum install -y epel-release
--- scripts/setup_ansible.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-yum install -y git gcc libffi-devel libselinux-python openssl-devel python-devel PyYAML python-pip ansible python2-jmespath bzip2
